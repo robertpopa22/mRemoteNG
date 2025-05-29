5cb07d003ca9c007edc0bbe589c35a56085eeeb8
Dimitrij <kvarkas@gmail.com>
2025-05-29 18:48:10 +0100

    UI changes for DB Setup

--- mRemoteNG/Properties/AssemblyInfo.cs
+++ mRemoteNG/Properties/AssemblyInfo.cs
@@ -18,10 +18,10 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("1.78.2.2947")]
-[assembly: AssemblyFileVersion("1.78.2.2947")]
+[assembly: AssemblyVersion("1.78.2.3019")]
+[assembly: AssemblyFileVersion("1.78.2.3019")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 2947)")]
+[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 3019)")]
 
 // Logging
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
