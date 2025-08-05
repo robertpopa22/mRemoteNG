19c954f9728abccea5877ce785a63014082c26ed
Dimitrij <kvarkas@gmail.com>
2025-08-05 15:33:16 +0100

    lib update

--- mRemoteNG/Properties/AssemblyInfo.cs
+++ mRemoteNG/Properties/AssemblyInfo.cs
@@ -18,10 +18,10 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version information
-[assembly: AssemblyVersion("1.78.2.3107")]
-[assembly: AssemblyFileVersion("1.78.2.3107")]
+[assembly: AssemblyVersion("1.78.2.3117")]
+[assembly: AssemblyFileVersion("1.78.2.3117")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 3107)")]
+[assembly: AssemblyInformationalVersion("1.78.2 (Nightly Build 3117)")]
 
 // Logging
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
